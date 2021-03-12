489f333af9eb20557122d19ba8aca36c41be4b97
Ivan Jaramillo <ivanciitojf@gmail.com>
2021-03-12 13:14:40 -0600

    Make a media queries section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+    const toggle = document.getElementById(toggleId)
+    const nav = document.getElementById(navId)
 
     if (toggle && nav) {
         toggle.addEventListener('click', () => {
