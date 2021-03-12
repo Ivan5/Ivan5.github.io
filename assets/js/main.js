0f4f01046a5121a911aa9ff4c191cc978aae3dec
Ivan Jaramillo <ivanciitojf@gmail.com>
2021-03-12 14:18:13 -0600

    Make a generate pdf works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,19 @@
-/*===== MENU SHOW =====*/
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId)
-    const nav = document.getElementById(navId)
+const areaCv = document.getElementById('area-cv')
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle', 'nav-menu')
+const resumeButton = document.getElementById('resume-button');
 
-/*===== REMOVE MENU MOBILE =====*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll('section[id]')
-
-window.addEventListener('scroll', scrollActive)
-
-function scrollActive() {
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
+const opt = {
+    margin: 0,
+    filename: 'IvanJaramilloCV.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 4 },
+    jsPDF: { format: 'a4', orientation: 'portrait' }
 }
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
+function generateResume() {
+    html2pdf(areaCv, opt)
+}
+
+resumeButton.addEventListener('click', function() {
+    generateResume();
 })
-
-/*SCROLL HOME*/
-sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', { delay: 200 })
-sr.reveal('.home__img', { origin: 'right', delay: 400 })
-
-/*SCROLL ABOUT*/
-sr.reveal('.about__img', { delay: 500 })
-sr.reveal('.about__subtitle', { delay: 300 })
-sr.reveal('.about__profession', { delay: 400 })
-sr.reveal('.about__text', { delay: 500 })
-sr.reveal('.about__social-icon', { delay: 600, interval: 200 })
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 })
-sr.reveal('.skills__img', { delay: 400 })
-
-/*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', { interval: 200 })
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', { interval: 200 })
-sr.reveal('.contact__input', { delay: 400 })
-sr.reveal('.contact__button', { delay: 600 })
